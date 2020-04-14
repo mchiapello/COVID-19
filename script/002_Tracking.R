988f045486b48642e9e138c71e2253d44d091914
Marco Chiapello <chiapello.m@gmail.com>
2020-04-14 07:31:59 +0200

    WIP

--- script/002_Tracking.R
+++ script/002_Tracking.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(here)
 library(lubridate)
 library(sf)
+library(gganimate) #Used for animations
+library(RColorBrewer) #Used for color scale 
 
 setwd(here())
 
@@ -21,3 +23,45 @@ it <- st_read("Italy_shapefile/ITA_adm/")
 sp <- co %>%
     left_join(it, by = c("DP" = "NAME_2")) %>%
     select(data:totale_casi, geometry)
+
+sp <- st_as_sf(sp) %>%
+    mutate(data = as.character(data), 
+           data = str_extract(data, "\\d\\d\\d\\d-\\d\\d-\\d\\d"), 
+           data = ymd(data))
+
+################################################################################
+# Plot
+# Makes plot with ggplot2 and gganimate to animate through the days 
+sp_map<-ggplot()+
+  geom_sf(data = it,fill = "white")+
+  geom_sf(data = sp %>%filter(data %in% c(ymd("2020-02-24"), ymd("2020-03-01"))),aes(fill=totale_casi))+
+  ggtitle("Spread of Covid19 Throughout Italy")+
+  xlab("")+
+  ylab("")+
+  labs(subtitle = "Date: {current_frame}",
+       caption = "Date Source: The New York Times\nAuthor: Kevin Zolea")+
+  cowplot::background_grid(major = "none", minor = "none") +
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+        axis.text.y = element_blank(), axis.ticks.y = element_blank(),
+        axis.line = element_blank(),
+        legend.background = element_blank(),
+        legend.position=c(-0.3,0.8),
+        plot.background = element_blank(),
+        panel.background = element_blank(),
+        legend.text = element_text(size=12),
+        legend.title = element_text(colour="black", size=12, face="bold"),
+        plot.title=element_text(size=20, face="bold",hjust =0.5),
+        plot.subtitle = element_text(hjust = 0.5,size=12),
+        plot.caption = element_text(size = 11,
+                                    hjust = .5,
+                                    color = "black",
+                                    face = "bold"))+
+  scale_fill_distiller("Number of Positive Cases",
+                       palette ="Reds",type = "div",
+                       direction = 1)+
+  transition_manual(data)
+
+#   animate(sp_map, nframe=25,fps = 2, end_pause = 15,height = 500, width =500)
+  animate(sp_map, renderer = ffmpeg_renderer())
+
+
